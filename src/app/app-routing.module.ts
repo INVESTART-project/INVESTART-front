c7e6c17083978bf702b833b806ba70c0d8fdb659
Qustio <qusitoof@gmail.com>
2023-03-15 19:12:06 +0300

    Base app and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IndexComponent } from './index/index.component';
+import { MainComponent } from './main/main.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MainComponent, title: 'Main Page' },
+  { path: 'index', component: IndexComponent, title: 'Index Page' },
+  { path: '**', component: NotFoundComponent, title: 'Page not found' },
+  // redirect to `first-component`
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { IndexComponent } from './index/index.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    IndexComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,

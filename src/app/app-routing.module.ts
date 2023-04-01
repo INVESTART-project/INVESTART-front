4e3492ed19207831f93873fe9cb1db79263d5e37
Qustio <qusitoof@gmail.com>
2023-04-01 15:42:03 +0300

    Removing Index component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { IndexComponent } from './index/index.component';
 import { MainComponent } from './main/main.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
   { path: '', component: MainComponent, title: 'Main Page' },
-  { path: 'index', component: IndexComponent, title: 'Index Page' },
   { path: '**', component: NotFoundComponent, title: 'Page not found' },
   // redirect to `first-component`
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
-import { IndexComponent } from './index/index.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainComponent,
-    IndexComponent,
     NotFoundComponent
   ],
   imports: [
